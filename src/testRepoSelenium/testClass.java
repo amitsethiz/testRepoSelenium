7322d82f01deec7d3fdeb0b399410eaeae3a39f4
Amit Sethi <only.amitsethi@gmail.com>
2022-06-14 15:24:10 -0600

    headless execution

--- src/testRepoSelenium/testClass.java
+++ src/testRepoSelenium/testClass.java
@@ -1,8 +1,9 @@
 package testRepoSelenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 
@@ -15,15 +16,23 @@ public class testClass {
 	
 	@Test
 	public void test() {
+		FirefoxBinary firefoxBinary = new FirefoxBinary();
+	    firefoxBinary.addCommandLineOptions("--headless");
+	    
+	    FirefoxOptions firefoxOptions = new FirefoxOptions();
+	    firefoxOptions.setBinary(firefoxBinary);
+	    FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+		
 		System.out.println ("Test Started");
-		// TODO Auto-generated method stub
+		
 
 		String baseURL = "https://www.google.com";
-		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.navigate().to(baseURL);
 		System.out.println (driver.getTitle());		
 		System.out.println ("Test Completed");
+		driver.quit();
 	}
 
 
