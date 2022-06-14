6bca1288371d1f630d20ea7ed6731f4cf3bec4cc
Amit Sethi <only.amitsethi@gmail.com>
2022-06-14 12:27:02 -0600

    testng changes

--- src/testRepoSelenium/testClass.java
+++ src/testRepoSelenium/testClass.java
@@ -9,22 +9,31 @@ import java.util.Scanner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 
 
 public class testClass {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	//	public static void main(String[] args) throws FileNotFoundException {
+	
+	
+	
+	@Test
+	public void test() {
 		System.out.println ("Test Started");
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.gecko.driver", "/home/amitsethi/eclipse-workspace/testProject/geckodriver");
+
 		String baseURL = "https://www.google.com";
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.navigate().to(baseURL);
 		System.out.println (driver.getTitle());		
 		System.out.println ("Test Completed");
-
 	}
+
+
+	//	}
 
 }
